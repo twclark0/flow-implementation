bbba5f3a466d3f06824e6d284096e09fb4dbe436
twclark0 <tylerc644@gmail.com>
2017-12-27 13:59:45 -0700

    Seperated todo component from App

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,11 @@
-// @flow
-
 import React, { Component } from 'react';
+import Todos from './Todo'
 import './App.css';
-import type { ComponentsProps } from '../flow-types'
 
-type State = {
-  count: number,
-  input: string
-}
-
-
-class App extends Component<ComponentsProps, State> {
-
-  state = {
-    count: 0,
-    input: ''
-  }
-
-  handleAddTodo = () => {
-    this.props.addTodo(this.state.input)
-    this.setState({ input: '' })
-  }
-  handleRemoveTodo = (i: number) => () => {
-    this.props.deleteTodo(i)
-  }
-  handleInputChange = (e: SyntheticKeyboardEvent<HTMLInputElement>) => {
-    this.setState({ input: e.currentTarget.value })
-  }
+class App extends Component {
   render() {
     return (
-      <div className='todo-container'>
-        <h1>Todo List</h1>
-        <ul className={'todo-ul'}>
-          {
-            this.props.list.todos.map((t, i) => (
-              <li className={'todo-li'} key={i} onClick={this.handleRemoveTodo(i)}>{t}</li>
-            ))
-          }
-        </ul>
-        <div className={'todo-inputContainer'}>
-          <input className={'todo-input'} type="text" onChange={this.handleInputChange} value={this.state.input} />
-          <button className={'todo-button'} onClick={this.handleAddTodo}>Click Me</button>
-        </div>
-      </div>
+        <Todos title={'My List'}/>
     )
   }
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,68 @@
+// @flow
+
+import React, { Component } from 'react';
+import './App.css';
+
+type Input = string
+
+type Id = number
+
+type AddTodo = { type: 'ADDTODO', data: Input }
+
+type RemoveTodo = { type: 'REMOVETODO', id: Id }
+
+type State = {
+  input: string,
+  todos: Array<string>
+}
+
+type Props = {
+    title: string
+}
+
+
+class App extends Component<Props, State> {
+
+  state = {
+    input: '',
+    todos: []
+  }
+
+  handleAddTodo = () => {
+    this.setState(prevState => ({
+        input: '',
+        todos: [...prevState.todos, prevState.input]
+    }))
+  }
+  handleRemoveTodo = (i: number) => () => {
+    this.setState(prevstate => ({
+        todos: [
+            ...prevstate.todos.slice(0, i),
+            ...prevstate.todos.slice(i + 1)
+        ]
+    }))
+  }
+  handleInputChange = (e: SyntheticKeyboardEvent<HTMLInputElement>) => {
+    this.setState({ input: e.currentTarget.value })
+  }
+  render() {
+    return (
+      <div className='todo-container'>
+        <h1>{this.props.title}</h1>
+        <ul className={'todo-ul'}>
+          {
+            this.state.todos.map((t, i) => (
+              <li className={'todo-li'} key={i} onClick={this.handleRemoveTodo(i)}>{t}</li>
+            ))
+          }
+        </ul>
+        <div className={'todo-inputContainer'}>
+          <input className={'todo-input'} type="text" onChange={this.handleInputChange} value={this.state.input} />
+          <button className={'todo-button'} onClick={this.handleAddTodo}>Click Me</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
